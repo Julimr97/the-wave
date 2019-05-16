f2d14eecf74e83eb46a70f533cb59da4e68c3740
Irene <UsuarioPC@MAKIWAZOWSKI>
2019-05-16 17:48:40 +0200

    he hecho el splash y la pagina de login , hay que linkear los botones entre los fragment , cuando creemos el siguiente , y hay que arreglar el nombre de usuario y contraseña

--- app/src/main/java/com/example/appgrupo/MainActivity.java
+++ app/src/main/java/com/example/appgrupo/MainActivity.java
@@ -10,4 +10,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public String uid;
+    public String nombre;
+    public String contraseña;
+
+    public MainActivity() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
