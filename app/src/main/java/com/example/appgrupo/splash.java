1bc9d7a83014869959f8c63821f218e7f9cb7117
Irene <UsuarioPC@MAKIWAZOWSKI>
2019-05-16 16:54:57 +0200

    he hecho el splash y la pagina de login , hay que linkear los botones entre los fragment , cuando creemos el siguiente , y hay que arreglar el nombre de usuario y contraseña

---
+++ app/src/main/java/com/example/appgrupo/splash.java
@@ -0,0 +1,33 @@
+package com.example.appgrupo;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity {
+
+    private final int duracion_splash = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent (splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },duracion_splash);
+
+    }
+}
