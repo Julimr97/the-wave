7ea289fdab45bae27c4c28410851c8fd38188791
Irene <UsuarioPC@MAKIWAZOWSKI>
2019-05-16 18:38:46 +0200

    he añadido el toolbar , hay que hacer un resize para arreglarlo y que no sea tan grande A demas el login no sale , sale directamente el inicio , cosa que habria que arreglar creo que con un if onclick button

--- app/src/main/java/com/example/appgrupo/MainActivity.java
+++ app/src/main/java/com/example/appgrupo/MainActivity.java
@@ -2,14 +2,53 @@ package com.example.appgrupo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.darwindeveloper.horizontalscrollmenulibrary.custom_views.HorizontalScrollMenuView;
+import com.darwindeveloper.horizontalscrollmenulibrary.extras.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    HorizontalScrollMenuView menu;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //ARREGLAR QUE SE QUITA LA PANTALLA DE LOGIN Y SALE LA DE INICIO DIRECTAMENTE
+        //CREO QUE SE ARREGLARIA CON UN ONCLICK BOTON LOGIN Y UN IF
         setContentView(R.layout.activity_main);
+
+
+        setContentView(R.layout.activity_inicio);
+
+        menu = (HorizontalScrollMenuView)findViewById(R.id.menu) ;
+        textView = (TextView)findViewById(R.id.txtText);
+
+        //creamos un metodo para añadir el menu
+        initMenu();
     }
+
+    private void initMenu() {
+        menu.addItem("Inicio",R.drawable.inicio);
+        menu.addItem("Calendario",R.drawable.ic_action_name);
+        menu.addItem("Añadir",R.drawable.mas);
+        menu.addItem("Informacion",R.drawable.informacion);
+
+        menu.setOnHSMenuClickListener(new HorizontalScrollMenuView.OnHSMenuClickListener() {
+            @Override
+            public void onHSMClick(MenuItem menuItem, int position) {
+                Toast.makeText(MainActivity.this, ""+menuItem.getText(), Toast.LENGTH_SHORT).show();
+                textView.setText(menuItem.getText());
+            }
+        });
+    }
+
+
+
+
 
     public String uid;
     public String nombre;
