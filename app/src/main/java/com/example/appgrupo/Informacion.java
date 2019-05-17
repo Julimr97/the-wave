a9c536bdfe50a23eed9ae7924a1681cd95a33a90
Irene <usuariopc@makiwazowski>
2019-05-17 09:28:21 +0200

    el login hay que arreglarlo para que funcione el guardar datos sale el login directamente hay que unir los fragment con botones juli ha hecho la pagina de informacion

---
+++ app/src/main/java/com/example/appgrupo/Informacion.java
@@ -0,0 +1,77 @@
+package com.example.appgrupo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class Informacion extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    ImageView imageView;
+    Button button2;
+    Button button3;
+    Button button4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textview);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        imageView = findViewById(R.id.imagen);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("HOLA PREDICCION");
+                textView.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.GONE);
+            }
+
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.ciclo));
+                textView.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageDrawable(getResources().getDrawable(R.drawable.ciclo));
+                textView.setVisibility(View.GONE);
+                imageView.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("HOLA PREDICCION");
+                textView.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.GONE);
+            }
+
+        });
+    }
+}
