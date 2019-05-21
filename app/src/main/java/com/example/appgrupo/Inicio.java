ec80b220a7e1b627d82e9114258f47f993f2a722
juli <juli97637@gmail.com>
2019-05-21 17:28:56 +0200

    FINAL DE APLICACION
    falta arreglar cosas minimas, cambiar calendario

--- app/src/main/java/com/example/appgrupo/Inicio.java
+++ app/src/main/java/com/example/appgrupo/Inicio.java
@@ -1,15 +1,24 @@
 package com.example.appgrupo;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.icu.text.IDNA;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class Inicio extends AppCompatActivity {
+import java.util.Calendar;
+
+public class Inicio extends AppCompatActivity implements View.OnClickListener {
 
     Button btnInicio,btnMas,btnCalendario,btnInfo;
+
+    Button btnFechaInicio, btnFechaFin;
+    EditText HoraTxt, HoraTxt2;
+    private int dia,mes,ano;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,46 @@ public class Inicio extends AppCompatActivity {
             }
         });
 
+        btnFechaInicio=(Button)findViewById(R.id.btnFechaInicio);
+        btnFechaFin=(Button)findViewById(R.id.btnFechaFin);
+        HoraTxt=(EditText)findViewById(R.id.HoraTxt);
+        HoraTxt2=(EditText)findViewById(R.id.HoraTxt2);
+        btnFechaInicio.setOnClickListener(this);
+        btnFechaFin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==btnFechaInicio){
+            final Calendar c= Calendar.getInstance();
+            dia= c.get(Calendar.DAY_OF_MONTH);
+            mes= c.get(Calendar.MONTH);
+            ano= c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    HoraTxt.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            }
+            ,dia,mes,ano);
+            datePickerDialog.show();
+        }
+        if (v==btnFechaFin){
+            final Calendar c= Calendar.getInstance();
+            dia= c.get(Calendar.DAY_OF_MONTH);
+            mes= c.get(Calendar.MONTH);
+            ano= c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    HoraTxt2.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            }
+                    ,dia,mes,ano);
+            datePickerDialog.show();
+        }
 
     }
 }
--- app/src/main/java/com/example/appgrupo/Mas.java
+++ app/src/main/java/com/example/appgrupo/Mas.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class Mas extends AppCompatActivity {
 
-
+    Button btnInicio,btnMas,btnCalendario,btnInfo;
 
     Button btmostrar, btexaminar;
     private EditText txtexaminar, txtdescripcion;
@@ -22,6 +22,30 @@ public class Mas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mas);
+        findViewById(R.id.btnhome).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Mas.this.startActivity(new Intent(Mas.this,Inicio.class));
+            }
+        });
+        findViewById(R.id.btncalendario).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Mas.this.startActivity(new Intent(Mas.this,Calendario.class));
+            }
+        });
+        findViewById(R.id.btnmas).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Mas.this.startActivity(new Intent(Mas.this,Mas.class));
+            }
+        });
+        findViewById(R.id.btninfo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Mas.this.startActivity(new Intent(Mas.this, Informacion.class));
+            }
+        });
 
         txtdescripcion = (EditText) findViewById(R.id.txtdescripcion);
         txtexaminar = (EditText) findViewById(R.id.txtexaminar);
